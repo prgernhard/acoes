ef0f8a78390dfe476751dbf5c4b84b4e0e11d5f1
prgernhard <prgernhard@gmail.com>
2023-05-21 13:43:30 -0300

    componente acao

---
+++ src/app/acoes/acoes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css']
+})
+export class AcoesComponent {
+
+}
